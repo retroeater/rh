85df65f6fe8db701ec073ab5f647ad364bf06541
retroeater <hirano@ryoei.net>
2020-05-31 10:34:48 +0900

    added navbar.js.

---
+++ navbar.js
@@ -0,0 +1,29 @@
+document.write(
+'<nav class="navbar navbar-expand-sm navbar-dark bg-dark mt-3 mb-3">' +
+	'<a class="navbar-brand" href="index.html">R</a>' +
+	'<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">' +
+		'<span class="navbar-toggler-icon"></span>' +
+	'</button>' +
+	'<div class="collapse navbar-collapse" id="navbarNavAltMarkup">' +
+		'<ul class="navbar-nav">' +
+			'<li class="nav-item"><a class="nav-link" href="maps.html">地図</a></li>' +
+			'<li class="nav-item"><a class="nav-link" href="work.html">仕事</a></li>' +
+			'<li class="nav-item"><a class="nav-link" href="mail.html">メール</a></li>' +
+			'<li class="nav-item"><a class="nav-link" href="dc.html">年金</a></li>' +
+			'<li class="nav-item"><a class="nav-link" href="credit.html">信用</a></li>' +
+			'<li class="nav-item"><a class="nav-link" href="health.html">健康</a></li>' +
+			'<li class="nav-item"><a class="nav-link" href="movies.html">映画</a></li>' +
+			'<li class="nav-item"><a class="nav-link" href="haruto.html">長男</a></li>' +
+			'<li class="nav-item"><a class="nav-link" href="aoto.html">次男</a></li>' +
+			'<li class="nav-item"><a class="nav-link" href="yura.html">長女</a></li>' +
+			'<li class="nav-item"><a class="nav-link" href="sara.html">次女</a></li>' +
+			'<li class="nav-item"><a class="nav-link" href="tweets.html">ツイート</a></li>' +
+		'</ul>' +
+	'</div>' +
+'</nav>' +
+
+'<!-- Bootstrap -- jQuery, Popper.js, Bootstrap JS -->' +
+'<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>' +
+'<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>' +
+'<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>'
+)
